2e1423b0615eae002bd41ec881e3725aafeba47b
Yash Gajjar <94056236+yashstar@users.noreply.github.com>
2022-08-30 18:42:29 +0530

    file1

---
+++ (part 9) function .py
@@ -0,0 +1,79 @@
+yash = "My name is yash!"
+print(type(yash))
+
+# This is function is used to see any space in my string or not!
+print(yash.isalnum())
+
+#this line declare some function.
+print(yash.isalpha())
+
+#This function is used for to ckeck any word/letter in bracket is come at end or not?
+print(yash.endswith("yash!"))
+
+#This function is used for to ckeck any word/letter is in line or not ?
+print(yash.count("a"))
+
+#This is function is used for any first letter is small then it capitalise only first letter
+print(yash.capitalize())
+
+#This function is used to ckeck or find an word in that line
+print(yash.find("name"))
+
+#this is function is used for get all capital to all lower letters
+print(yash.lower())
+
+#this is function creates all lower to all upper
+print(yash.upper())
+
+#This is function is used to replace any in sentence!
+print(yash.replace("name" , "find"))
+
+# All String function in python
+
+# capitalize()->	Converts the first character to upper case
+# casefold()  ->	Converts string into lower case
+# center()	  ->    Returns a centered string
+# count()	  ->    Returns the number of times a specified value occurs in a string
+# encode()    ->  	Returns an encoded version of the string
+# endswith()  ->	Returns true if the string ends with the specified value
+# expandtabs()-> 	Sets the tab size of the string
+# find()	  ->    Searches the string for a specified value and returns the position of where it was found
+# format()	  ->    Formats specified values in a string
+# format_map()-> 	Formats specified values in a string
+# index()	  ->    Searches the string for a specified value and returns the position of where it was found
+# isalnum()   ->	Returns True if all characters in the string are alphanumeric
+# isalpha()   ->	Returns True if all characters in the string are in the alphabet
+# isascii()   ->	Returns True if all characters in the string are ascii characters
+# isdecimal() ->	Returns True if all characters in the string are decimals
+# isdigit()   ->  	Returns True if all characters in the string are digits
+# isidentifier()-> 	Returns True if the string is an identifier
+# islower()	  ->    Returns True if all characters in the string are lower case
+# isnumeric() -> 	Returns True if all characters in the string are numeric
+# isprintable()->	Returns True if all characters in the string are printable
+# isspace()   ->    Returns True if all characters in the string are whitespaces
+# istitle()   ->	Returns True if the string follows the rules of a title
+# isupper()	  ->    Returns True if all characters in the string are upper case
+# join()	  ->    Joins the elements of an iterable to the end of the string
+# ljust()	  ->    Returns a left justified version of the string
+# lower()	  ->    Converts a string into lower case
+# lstrip()    ->    Returns a left trim version of the string
+# maketrans() -> 	Returns a translation table to be used in translations
+# partition() -> 	Returns a tuple where the string is parted into three parts
+# replace()	  ->    Returns a string where a specified value is replaced with a specified value
+# rfind()	  ->Searches the string for a specified value and returns the last position of where it was found
+# rindex()    ->Searches the string for a specified value and returns the last position of where it was found
+# rjust()	  ->    Returns a right justified version of the string
+# rpartition()->  	Returns a tuple where the string is parted into three parts
+# rsplit()    -> 	Splits the string at the specified separator, and returns a list
+# rstrip()	  ->    Returns a right trim version of the string
+# split()	  ->    Splits the string at the specified separator, and returns a list
+# splitlines()->	Splits the string at line breaks and returns a list
+# startswith()->	Returns true if the string starts with the specified value
+# strip()     ->    Returns a trimmed version of the string
+# swapcase()  ->	Swaps cases, lower case becomes upper case and vice versa
+# title()     ->   	Converts the first character of each word to upper case
+# translate()-> 	Returns a translated string
+# upper()	  ->    Converts a string into upper case
+# zfill()	  ->    Fills the string with a specified number of 0 values at the beginning
+ 
+
